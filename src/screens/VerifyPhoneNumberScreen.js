558aa563f84d232921a54c22b538d40e8b0d1916
Nipun007 <dexter.nipun@gmail.com>
2024-05-20 02:19:39 +0600

    Verify Phone Number Screen on progress... Flag added.

--- src/screens/VerifyPhoneNumberScreen.js
+++ src/screens/VerifyPhoneNumberScreen.js
@@ -1,18 +1,44 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import CustomButton from '../components/CustomButton'; // Ensure the path is correct
 import CustomInputTextField from '../components/CustomInputTextField'; // Import the new CustomInputTextField
+import { Picker } from '@react-native-picker/picker';
 
 const VerifyPhoneNumber = () => {
+  const [selectedCountryCode, setSelectedCountryCode] = useState('+61');
+  const [countryCode, setCountryCode] = useState('+61');
   const [phoneNumber, setPhoneNumber] = useState('');
+
+  const countryCodes = [
+    { label: 'Australia (+61)', value: '+61', flag: '🇦🇺' },
+    { label: 'USA (+1)', value: '+1', flag: '🇺🇸' },
+    { label: 'UK (+44)', value: '+44', flag: '🇬🇧' },
+    { label: 'India (+91)', value: '+91', flag: '🇮🇳' },
+
+    // Add more country codes as needed
+  ];
 
   const handleSubmit = () => {
     // Handle submit logic here
-    console.log('Phone Number Submitted:', phoneNumber);
+    console.log('Phone Number Submitted:', selectedCountryCode + phoneNumber);
   };
 
   return (
     <View style={styles.container}>
+      <Picker
+        selectedValue={selectedCountryCode}
+        style={styles.picker}
+        onValueChange={(itemValue) => setSelectedCountryCode(itemValue)}
+      >
+        {countryCodes.map((code) => (
+          <Picker.Item
+            key={code.value}
+            label={`${code.flag}`}
+            value={code.value}
+            style={styles.pickerItem}
+          />
+        ))}
+      </Picker>
       <CustomInputTextField
         placeholder="Enter your phone number"
         keyboardType="phone-pad"
@@ -24,10 +50,7 @@ const VerifyPhoneNumber = () => {
         industry.
       </Text>
       <View style={styles.buttonContainer}>
-        <CustomButton 
-          label="Continue" 
-          onPress={handleSubmit} 
-        />
+        <CustomButton label="Continue" onPress={handleSubmit} />
       </View>
     </View>
   );
@@ -48,6 +71,15 @@ const styles = StyleSheet.create({
   buttonContainer: {
     width: '100%',
     marginTop: 20,
+  },
+  picker: {
+    width: 118,
+    height: 50,
+    borderRightWidth: 1,
+    borderColor: '#ccc',
+  },
+  pickerItem: {
+    fontSize: 30,
   },
 });
 
