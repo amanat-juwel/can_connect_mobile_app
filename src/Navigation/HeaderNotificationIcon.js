89a7774b74b690a6d5541b2502020fbc7b1f18e3
faham.dev <faham.dev@gmail.com>
2024-06-30 00:50:12 +0600

    Add notification count

--- src/Navigation/HeaderNotificationIcon.js
+++ src/Navigation/HeaderNotificationIcon.js
@@ -1,20 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import colors from '../constants/colors';
 import routes from './routes';
+import commonApi from '../api/common';
 
-export const HeaderNotificationIcon = () => {
+export const HeaderNotificationIcon = ({}) => {
+  const [notificationCount, setNotificationCount] = useState(0);
   const navigation = useNavigation();
+
+  const getNotificationCount = async () => {
+    const result = await commonApi.getNotificationCount();
+    if (result.ok && result.data.success) {
+      setNotificationCount(result.data.data.pending_notification);
+    }
+  };
+
+  useEffect(() => {
+    getNotificationCount();
+  }, []);
+
   return (
     <TouchableOpacity
       onPress={() => navigation.navigate(routes.NOTIFICATION_SCREEN)}
+      style={{ marginRight: 10 }}
     >
-      <Ionicons
-        name="notifications-outline"
-        size={30}
-        style={{ color: colors.white }}
-      />
+      <View style={{ position: 'relative' }}>
+        <Ionicons
+          name="notifications-outline"
+          size={30}
+          style={{ color: colors.white }}
+        />
+        {notificationCount > 0 && (
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              backgroundColor: colors.white,
+              borderRadius: 10,
+              width: 18,
+              height: 18,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={{ color: colors.primary, fontSize: 12 }}>
+              {notificationCount}
+            </Text>
+          </View>
+        )}
+      </View>
     </TouchableOpacity>
   );
 };
--- src/api/common.js
+++ src/api/common.js
@@ -1,10 +1,15 @@
 import client from './client';
 
 const getNotificationsEndpoint = '/notification';
+const getNotificationCountEndpoint = '/notification-count';
 const getDashboardDataEndpoint = '/dashboard';
 
 const getNotifications = () => {
   return client.get(getNotificationsEndpoint);
+};
+
+const getNotificationCount = () => {
+  return client.get(getNotificationCountEndpoint);
 };
 
 const getDashboardData = () => {
@@ -13,5 +18,6 @@ const getDashboardData = () => {
 
 export default {
   getNotifications,
+  getNotificationCount,
   getDashboardData,
 };
