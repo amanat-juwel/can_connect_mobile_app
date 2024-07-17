0e98c20d9a596765f82e34c5b52d9024dc126198
faham.dev <faham.dev@gmail.com>
2024-07-17 19:57:22 +0600

    Add history details screen

--- src/api/requestor.js
+++ src/api/requestor.js
@@ -2,6 +2,7 @@ import client from './client';
 
 const storeRequestEndpoint = '/store-request';
 const requestTrailEndpoint = '/track-request';
+const cancelRequestEndpoint = '/update-request-status';
 
 const storeRequest = (payload) => {
   return client.post(storeRequestEndpoint, payload);
@@ -11,7 +12,11 @@ const getRequestTrail = (sku) => {
   return client.post(requestTrailEndpoint, sku);
 };
 
+const cancelRequest = (payload) => {
+  return client.patch(cancelRequestEndpoint, payload);
+};
 export default {
   storeRequest,
   getRequestTrail,
+  cancelRequest,
 };
--- src/screens/HistoryDetailsScreen.js
+++ src/screens/HistoryDetailsScreen.js
@@ -3,13 +3,15 @@ import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import CustomButton from '../components/CustomButton';
 import { useTranslation } from 'react-i18next';
 import colors from '../constants/colors';
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 import useAuth from '../auth/useAuth';
 import userType from '../constants/userType';
 import requestorApi from '../api/requestor';
+import collectorApi from '../api/Collector';
 import { Icon } from 'react-native-elements';
+import routes from '../Navigation/routes';
 
-const HistoryDetailsScreen = ({ route }) => {
+const HistoryDetailsScreen = ({ route, navigation }) => {
   const request = route.params?.request;
   const [requestTrail, setRequestTrail] = useState();
   const { t } = useTranslation();
@@ -30,6 +32,32 @@ const HistoryDetailsScreen = ({ route }) => {
       completed: colors.darkGreen,
     };
     return colorMap[request.status];
+  };
+
+  const cancelRequest = async () => {
+    const result = await requestorApi.cancelRequest({
+      sku: request.sku,
+      status: 'cancelled',
+    });
+    if (result.ok && result.data.success) {
+      navigation.navigate({
+        name: routes.HISTORY_SCREEN,
+        key: `${routes.HISTORY_SCREEN}-${Date.now()}`,
+      });
+    }
+  };
+
+  const completeRequest = async () => {
+    const result = await collectorApi.completeRequest({
+      sku: request.sku,
+      status: 'completed',
+    });
+    if (result.ok && result.data.success) {
+      navigation.navigate({
+        name: routes.HISTORY_SCREEN,
+        key: `${routes.HISTORY_SCREEN}-${Date.now()}`,
+      });
+    }
   };
 
   useEffect(() => {
@@ -87,28 +115,112 @@ const HistoryDetailsScreen = ({ route }) => {
           </View>
         </View>
         <View style={styles.textContainer}>
+          <Text style={styles.headingLabel}>{t('ItemListText')}</Text>
+        </View>
+        <View style={styles.leftAlignedContainer}>
+          {request.items.map((item) => (
+            <View key={item.item_id} style={styles.row}>
+              <MaterialCommunityIcons
+                name="recycle-variant"
+                size={18}
+                color={colors.primary}
+              />
+              <Text
+                style={styles.text}
+              >{`${item.qty} ${item.unit} of ${item.type}`}</Text>
+            </View>
+          ))}
+          {request.is_donation === 1 && (
+            <View style={[styles.row, { marginTop: 10 }]}>
+              <MaterialIcons
+                name="volunteer-activism"
+                size={18}
+                color={colors.primary}
+              />
+              <Text style={styles.text}>{t('DonationText')}</Text>
+            </View>
+          )}
+          {request.note?.length > 0 && (
+            <View style={[styles.row, { marginTop: 10 }]}>
+              <MaterialIcons name="note" size={18} color={colors.primary} />
+              <Text style={styles.text}>{request.note}</Text>
+            </View>
+          )}
+        </View>
+        {requestTrail && requestTrail.length > 0 && (
+          <View style={styles.textContainer}>
+            <Text style={styles.headingLabel}>{t('TrackText')}</Text>
+          </View>
+        )}
+        {requestTrail && requestTrail.length > 0 && (
+          <View style={styles.leftAlignedContainer}>
+            {requestTrail.map((item) => (
+              <View key={item.created_at} style={styles.trackRow}>
+                <View style={styles.tractIcon}>
+                  <MaterialCommunityIcons
+                    name="check-circle"
+                    size={18}
+                    color={colors.primary}
+                  />
+                </View>
+
+                <View style={styles.trackTextContainer}>
+                  <Text style={styles.text}>{item.readable_created_time}</Text>
+                  <Text style={styles.text}>{item.text}</Text>
+                </View>
+              </View>
+            ))}
+          </View>
+        )}
+        <View style={styles.textContainer}>
           <Text style={styles.headingLabel}>{t('ContactDetailsText')}</Text>
         </View>
-        <View>
-          <View style={styles.leftAlignedContainer}>
-            <View style={styles.row}>
-              <MaterialIcons
-                name="calendar-today"
-                size={16}
-                color={colors.black}
-              />
-              <Text style={styles.text}>{request.requestor.first_name}</Text>
-            </View>
+        <View style={styles.leftAlignedContainer}>
+          <View style={styles.row}>
+            <MaterialIcons
+              name="account-circle"
+              size={18}
+              color={colors.black}
+            />
+            <Text
+              style={styles.text}
+            >{`${request.requestor.first_name} ${request.requestor.last_name}`}</Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialIcons name="phone" size={18} color={colors.black} />
+            <Text style={styles.text}>{`${request.requestor.phone}`}</Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialIcons name="email" size={18} color={colors.black} />
+            <Text style={styles.text}>{`${request.requestor.email}`}</Text>
+          </View>
+          <View style={styles.row}>
+            <MaterialIcons name="location-pin" size={18} color={colors.black} />
+            <Text
+              style={styles.text}
+            >{`${request.street_address}, ${request.city.name}, ${request.state.name}, ${request.postal_code}`}</Text>
           </View>
         </View>
       </View>
 
-      <View style={styles.bottomView}>
-        <CustomButton
-          label={t('pickupAppointmentButtonText')}
-          onPress={() => {}}
-        />
-      </View>
+      {user && user?.category === userType.REQUESTOR ? (
+        <View style={styles.bottomView}>
+          <CustomButton
+            label={t('CancelRequestText')}
+            color={colors.red}
+            onPress={cancelRequest}
+            disabled={request.status === 'cancelled'}
+          />
+        </View>
+      ) : (
+        <View style={styles.bottomView}>
+          <CustomButton
+            label={t('CompleteRequestText')}
+            onPress={completeRequest}
+            disabled={request.status === 'completed'}
+          />
+        </View>
+      )}
     </ScrollView>
   );
 };
@@ -119,13 +231,13 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
     paddingVertical: 20,
+    paddingHorizontal: 10,
   },
   container: {
     justifyContent: 'flex-start',
     alignItems: 'center',
     padding: 10,
   },
-
   textHeader: {
     fontSize: 22,
     fontWeight: 'bold',
@@ -160,11 +272,11 @@ const styles = StyleSheet.create({
   },
   dateContainer: {
     flex: 1,
-    alignItems: 'left',
+    alignItems: 'center',
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderWidth: 1,
-    borderColor: '#00A75A',
+    borderColor: colors.primary,
     borderRadius: 10,
     marginHorizontal: 10,
   },
@@ -175,7 +287,7 @@ const styles = StyleSheet.create({
   },
   dateContent: {
     flexDirection: 'row',
-    alignItems: 'left',
+    alignItems: 'center',
     marginTop: 5,
   },
   icon: {
@@ -205,9 +317,11 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   headingLabel: {
-    fontSize: 25,
+    fontSize: 22,
     fontWeight: '500',
     marginTop: 10,
+    marginBottom: 5,
+    color: colors.primary,
   },
   row: {
     flexDirection: 'row',
@@ -217,12 +331,23 @@ const styles = StyleSheet.create({
   text: {
     marginStart: 8,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: '500',
     color: colors.black,
   },
   leftAlignedContainer: {
     alignSelf: 'flex-start',
-    paddingHorizontal: 10,
+  },
+  trackRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  tractIcon: {
+    alignItems: 'center',
+  },
+  trackTextContainer: {
+    marginStart: 8,
+    flexDirection: 'column',
   },
 });
 
