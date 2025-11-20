b635cdef629d325fad7403a4cefa53ea417785bd
Amanat Juwel <juwel@bluetechbd.com>
2025-11-20 23:57:54 +0600

    required street address on profile edit

--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -29,7 +29,7 @@ const validationSchema = Yup.object().shape({
   //state: Yup.object().required(),
   //city: Yup.object().required(),
   //postal_code: Yup.string(),
-  //street_address: Yup.string().required(),
+  street_address: Yup.string().required(),
 });
 
 const EditProfileScreen = () => {
